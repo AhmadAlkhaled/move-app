69d2ea622bcc8732c51e7d21972966b5a55376a0
Alkhaled <almzrab@gmail.com>
2022-04-10 20:40:44 +0200

    add

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Weather } from './component/wether';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Weather/>
+      
+    </div>
+  );
+}
+
+export default App;
